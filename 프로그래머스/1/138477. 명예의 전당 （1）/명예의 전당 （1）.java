8efdda5bd891299a9139c076e428db24849a2aa1
Ksr-ccb <harang4282@gmail.com>
2025-03-24 09:49:21 +0900

    [level 1] Title: 명예의 전당 (1), Time: 10.04 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Collections;
 
@@ -8,28 +7,14 @@ class Solution {
         
         List<Integer> kValues = new ArrayList<>();
         int[] answer = new int[score.length];
-        
-        if( k > score.length ){
-            for(int i = 0; i < score.length; i++){
-                kValues.add(score[i]);
-                Collections.sort(kValues);
-                answer[i] = Collections.min(kValues);
-            }    
-            
-            return answer;
-        }
-        
-
-        
-        
+ 
         for (int i = 0; i < score.length; i++) {
             kValues.add(score[i]);
+            Collections.sort(kValues);
             
             if (kValues.size() > k) {
-                Collections.sort(kValues);
                 kValues.remove(0);
             }
-            
             answer[i] = Collections.min(kValues);
         }
         
