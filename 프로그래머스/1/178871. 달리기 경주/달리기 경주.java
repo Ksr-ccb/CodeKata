6de149822a3ab3297c7958abf9bc48b4a28b5521
Ksr-ccb <harang4282@gmail.com>
2025-04-21 09:42:50 +0900

    [level 1] Title: 달리기 경주, Time: 509.67 ms, Memory: 384 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = players;
+        
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            map.put(players[i], i);
+        }
+        
+        for (String calling : callings) {
+            int index = map.get(calling);
+            
+            map.put(calling, index-1);
+            map.put(answer[index-1], index);
+            
+            answer[index] = answer[index-1];
+            answer[index - 1] = calling;
+        }
+        
+        return answer;
+    }
+}
